4bcaaa8566247ccf30f9df8f2f10bfbc642bde91
chinmay khede <chinmaykhede@gmail.com>
2026-09-10 18:32:00 +0530

    Done

---
+++ Java_Collection/src/com/rays/autoboxing/unboxing/TestAutoBoxing.java
@@ -0,0 +1,10 @@
+package com.rays.autoboxing.unboxing;
+//AutoBoxing is the automatic 
+public class TestAutoBoxing {
+	public static void main(String[] args) {
+		int a = 10;
+		Integer i = a;// AutoBoxing 
+		System.out.println(a);
+	}
+
+}
---
+++ Java_Collection/src/com/rays/autoboxing/unboxing/TestUnBoxing.java
@@ -0,0 +1,10 @@
+package com.rays.autoboxing.unboxing;
+
+public class TestUnBoxing {
+	public static void main(String[] args) {
+		Integer i = 56;
+		int a = i;// UnBoxing
+		System.out.println(a);
+	}
+
+}
